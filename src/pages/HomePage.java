cf24837acb25fa13b5116d4538f78c48e870adfb
swapnavinvishwanath <swapnavinladhwe.gmail.com>
2024-04-26 23:33:38 +0530

    first commit

---
+++ src/pages/HomePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class HomePage {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+}
---
+++ src/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class LoginPage {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+
+	}
+
+}
